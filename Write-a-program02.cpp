a1e5ba6dbcc09f0e6bc0297a3b10a951f6480f38
vongatetchinda-max <vongatetchinda@gmail.com>
2026-01-28 04:54:30 -0800

    Add files via upload

---
+++ Write-a-program02.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	printf("1*********");
+	printf("\n12********");
+	printf("\n123*******");
+	printf("\n1234******");
+	printf("\n12345*****");
+	return 0;
+}
---
+++ Write-a-program04.cpp
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+	printf("The year you were born: %d - %d = %d\n",2026,19,2026-19);
+	return 0;
+}
